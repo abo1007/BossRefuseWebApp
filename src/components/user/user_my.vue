7c252108363952ca6e715adade1f76d197861f63
abo1007 <1003208172@qq.com>
2020-08-03 20:09:52 +0800

    Update user_my.vue

--- src/components/user/user_my.vue
+++ src/components/user/user_my.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="user-my">
-        <h1>my</h1>
+        <div class="header">
+            <van-icon name="setting-o" color="#fff" size="30" class="icon"/>
+        </div>
+        <div class="user">
+
+        </div>
         <van-button type="primary" @click="goback">主页</van-button>
     </div>
 </template>
@@ -21,6 +26,22 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+#user-my{
+    .header{
+        height:40px;
+        width:100%;
+        background-color:#55cac4;
+        display:flex;
+        align-items:center;
+        .icon{
+            position:absolute;
+            right:10px;
+        }
+    }
+    .user{
+        height:100px;
+        background-color:#55cac4;
+    }
+}
 </style>
