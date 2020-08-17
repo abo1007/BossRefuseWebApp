1c8836fa4e1fe36552db8dc9a2c63e7a8d5e87f2
abo1007 <1003208172@qq.com>
2020-08-17 16:51:16 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,20 @@ import user_focus from "../components/user/user_focus"
 import user_msg from "../components/user/user_msg"
 import user_my from "../components/user/user_my"
 
+import Design from "../components/user_category/Design"
+import market from "../components/user_category/market"
+import more from "../components/user_category/more"
+import operate from "../components/user_category/operate"
+import product from "../components/user_category/product"
+import technology from "../components/user_category/technology"
+
 import com from "../components/company/com"
 import com_home from "../components/company/com-home"
 import com_focus from  "../components/company/com-focus"
 import com_msg from "../components/company/com-msg"
 import com_my from "../components/company/com-my"
+
+
 
 import err404 from "../components/404"
 
@@ -30,12 +39,18 @@ Vue.use(VueRouter);
     component: user,
     redirect:'/user/home',
     children:[
-      { path: '/user/home', name: 'user_home', component: user_home},
+      { path: '/user/home', name: 'user_home', component: user_home },
       { path: '/user/focus', name: 'user_focus', component: user_focus},
       { path: '/user/msg', name: 'user_msg', component: user_msg},
       { path: '/user/my', name: 'user_my', component: user_my}
     ]
   },
+    { path: '/user/home/design', name: 'design', component: Design },
+    { path: '/user/home/market', name: 'market', component: market },
+    { path: '/user/home/more', name: 'more', component: more },
+    { path: '/user/home/operate', name: 'operate', component: operate },
+    { path: '/user/home/product', name: 'product', component: product },
+    { path: '/user/home/technology', name: 'technology', component: technology },
   {
     path: '/com',
     name:'com',
