e2ee3c02d0a70c19d1e177af1654950d35f99721
abo1007 <1003208172@qq.com>
2020-10-25 09:29:39 +0800

    update

--- src/components/company/com-msg.vue
+++ src/components/company/com-msg.vue
@@ -1,12 +1,48 @@
 <template>
     <div id="com-msg">
-        <h1>com-msg</h1>
+        <van-nav-bar :title="getMsgTitle()" />
+        <van-pull-refresh
+                v-model="isLoading"
+                success-text="刷新成功"
+                @refresh="onRefresh"
+                style="text-align:center;"
+                class="msg-container"
+        >
+            <!--                <p>刷新次数: {{ count }}</p>-->
+            <msg-item :info="MsgTest"/>
+            <msg-item :info="MsgTest"/>
+        </van-pull-refresh>
     </div>
 </template>
 
 <script>
+import msg_item from "../child/msg_item";
     export default {
-        name: "com-msg"
+        name: "com-msg",
+        data(){
+            return{
+                msgNum:20,
+                isLoading:false,
+                count:0,
+                MsgTest:{
+                    url:"http://localhost/boss.jpg",name:'AAA廊坊富士康 人事经理',time:'23:08',content:'我们这试学一月不收取任何费用...'
+                }
+            }
+        },
+        methods:{
+            getMsgTitle(){
+                return '消息('+this.msgNum+')'
+            },
+            onRefresh() {
+                setTimeout(() => {
+                    this.isLoading = false;
+                    this.count++;
+                }, 1000);
+            },
+        },
+        components:{
+            'msg-item':msg_item
+        }
     }
 </script>
 
--- src/components/company/com-my.vue
+++ src/components/company/com-my.vue
@@ -1,6 +1,40 @@
 <template>
     <div id="com-my">
-        <h1>com-my</h1>
+        <div class="header">
+            <van-icon name="setting-o" color="#fff" size="30" class="icon" @click="goOption"/>
+        </div>
+        <div class="user">
+            <div class="left">
+                <div class="img">
+                    <img src="http://localhost/boss.jpg" alt="">
+                </div>
+            </div>
+            <div class="right">
+                <p class="username">老杨同志Abo</p>
+                <p class="level">一切的伟大，源自一个勇敢的开始</p>
+            </div>
+        </div>
+        <div class="userdata">
+            <div class="view">
+                <p class="num">{{userdata.chat}}</p>
+                <p class="name">沟通中</p>
+            </div>
+            <div class="view">
+                <p class="num">{{userdata.interview}}</p>
+                <p class="name">待面试</p>
+            </div>
+            <div class="view">
+                <p class="num">{{userdata.offer}}</p>
+                <p class="name">录用</p>
+            </div>
+            <div class="view">
+                <p class="num">{{userdata.star}}</p>
+                <p class="name">收藏</p>
+            </div>
+        </div>
+        <div class="userinfo" @click="InfoMore">
+            <van-icon name="ellipsis" color="#fff" size="30" />
+        </div>
         <van-button type="primary" @click="goback">主页</van-button>
     </div>
 </template>
@@ -10,17 +44,101 @@
         name: "com-my",
         data(){
             return{
-
+                userdata:{
+                    chat:0,interview:0,offer:0,star:0
+                }
             }
         },
         methods:{
             goback(){
                 this.$router.push('../')
+            },
+            goOption(){
+                this.$router.push({ name: "user_option" })
             }
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    #com-my{
+        .header{
+            height:40px;
+            width:100%;
+            background-color:#55cac4;
+            display:flex;
+            align-items:center;
+            .icon{
+                position:absolute;
+                right:10px;
+            }
+        }
+        .user{
+            height:100px;
+            background-color:#55cac4;
+            display:flex;
+            padding:20px;
+            .left{
+                flex:3;
+                display:flex;
+                align-items:center;
+                justify-content:center;
+                .img{
+                    height:80px;
+                    width:80px;
+                    background-color: #fff;
+                    border:1px solid #fff;
+                    img{
+                        width:100%;
+                        height:100%;
+                    }
+                }
+            }
+            .right{
+                flex:7;
+                color:#fff;
+                .username{
+                    margin-top:20px;
+                    margin-bottom:0;
+                    font-size:1.2em;
+                }
+                .level{
+                    margin:5px 0 0 0;
+                }
+            }
+        }
+        .userdata{
+            height:80px;
+            background-color:#55cac4;
+            display:flex;
+            padding:10px;
+            .view{
+                flex:1;
+                display:flex;
+                /*text-align:center;*/
+                align-items:center;
+                flex-direction:column;
+                padding:20px 0;
 
+                p{
+                    margin:0;
+                    font-family: 微软雅黑;
+                    color:#fff;
+                }
+                .num{
+                    font-size:18px;
+                    font-weight:800;
+                }
+                .name{
+                    font-size:14px;
+                }
+            }
+        }
+        .userinfo{
+            height:30px;
+            background-color:#55cac4;
+            display:flex;
+            justify-content:center;
+        }
+    }
 </style>
