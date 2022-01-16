498b2ba851026265dfbe59fe4b475a777e48b7ee
bo yang <1003208172@qq.com>
2022-01-16 19:30:44 +0800

    update

--- src/components/view/about.vue
+++ src/components/view/about.vue
@@ -1,106 +1,120 @@
 <template>
-    <div id="about-container">
-        <div class="header">
-            <van-icon name="arrow-left" color="#fff" size="30" class="icon" @click="goback"/>
-            <p>关于</p>
-        </div>
-        <img src="../../assets/slide1.png" alt="Logo" @click="egg">
-        <div class="content" ref="content">
-            <h1>Boss直拒</h1>
-            <p>您身边的求职专家</p>
-            <p>版本 0.9.0</p>
-            <p>Yangbo@2021</p>
-            <transition>
-                <p v-show="show" class="hi">Yangbo & Yueying</p>
-            </transition>
-        </div>
+  <div id="about-container">
+    <div class="header">
+      <van-icon name="arrow-left" color="#fff" size="30" class="icon" @click="goback"/>
+      <p>关于</p>
     </div>
+    <img src="../../assets/slide1.png" alt="Logo" @click="egg">
+    <div class="content" ref="content">
+      <h1>Boss直拒</h1>
+      <p>您身边的求职专家</p>
+      <p>版本 0.9.0</p>
+      <p>Yangbo@2021</p>
+      <p>Yangbo@2022</p>
+
+      <transition>
+        <p v-show="show" class="hi">Yangbo & Yueying</p>
+      </transition>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "about",
-        data(){
-          return{
-              from:this.$store.state.currentPosition,
-              show:false,
-              showi:0
-          }
-        },
-        methods:{
-            goback(){
-                this.$router.push("/" + this.from + "/my");
-            },
-            egg(){
-                if(this.showi >= 5){
-                   this.addegg();
-                }else{
-                    this.$toast("还有"+(5-this.showi)+"次");
-                    this.showi++;
-                }
-            },
-            addegg(){
-                this.show = true;
-            }
-
-        }
+export default {
+  name: "about",
+  data() {
+    return {
+      from: this.$store.state.currentPosition,
+      show: false,
+      showi: 0
     }
+  },
+  methods: {
+    goback() {
+      this.$router.push("/" + this.from + "/my");
+    },
+    egg() {
+      if (this.showi >= 5) {
+        this.addegg();
+      } else {
+        this.$toast("还有" + (5 - this.showi) + "次");
+        this.showi++;
+      }
+    },
+    addegg() {
+      this.show = true;
+    }
+
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    #about-container{
-        font-family: 微软雅黑;
-        .header{
-            height:50px;
-            width:100%;
-            background-color:#55cac4;
-            display:flex;
-            align-items:center;
-            .icon{
-                position:absolute;
-                left:10px;
-            }
-            p{
-                text-align:center;
-                width:100%;
-                font-size:18px;
-                margin:0;
-                color:#fff;
-                font-weight:500;
-            }
-        }
-        img{
-            width:100%;
-        }
-        .content{
-            text-align: center;
-            h1{
-                font-weight:100;
-                margin:15px 0;
-            }
-            p{
-                font-weight:100;
-            }
-            .show{
-                transition:all 0.4s ease;
-                display:block;
+#about-container {
+  font-family: 微软雅黑;
 
-            }
-            .fade-enter-active, .fade-leave-active {
-                transition: opacity .5s;
-            }
-            .fade-enter, .fade-leave-to{
-                opacity: 0;
-            }
-            .hi{
-                font-size:18px;
-                user-select: none;
-            }
-            .hi:active{
-                transition:all 0.1s ease;
-                font-size:20px;
-                color:red;
-            }
-        }
+  .header {
+    height: 50px;
+    width: 100%;
+    background-color: #55cac4;
+    display: flex;
+    align-items: center;
+
+    .icon {
+      position: absolute;
+      left: 10px;
     }
+
+    p {
+      text-align: center;
+      width: 100%;
+      font-size: 18px;
+      margin: 0;
+      color: #fff;
+      font-weight: 500;
+    }
+  }
+
+  img {
+    width: 100%;
+  }
+
+  .content {
+    text-align: center;
+
+    h1 {
+      font-weight: 100;
+      margin: 15px 0;
+    }
+
+    p {
+      font-weight: 100;
+    }
+
+    .show {
+      transition: all 0.4s ease;
+      display: block;
+
+    }
+
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s;
+    }
+
+    .fade-enter, .fade-leave-to {
+      opacity: 0;
+    }
+
+    .hi {
+      font-size: 18px;
+      user-select: none;
+    }
+
+    .hi:active {
+      transition: all 0.1s ease;
+      font-size: 20px;
+      color: red;
+    }
+  }
+}
 </style>
--- src/components/view/service.vue
+++ src/components/view/service.vue
@@ -1,53 +1,65 @@
 <template>
-    <div id="service-container">
-        <bo-navbar text="客服" @left-fun="goback"/>
-        <van-card
-                desc="负责线上问题投诉建议等处理"
-                title="客服中心"
-                thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
-        >
-            <template #tags>
-                <van-tag plain type="success">线上问题</van-tag>
-            </template>
-            <template #footer>
-                <van-button size="mini">400-820-8820</van-button>
-            </template>
-        </van-card>
-        <van-card
-                desc="诈骗等违法线下问题处理"
-                title="防诈中心"
-                thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
-        >
-            <template #tags>
-                <van-tag plain type="danger">欺诈问题</van-tag>
-            </template>
-            <template #footer>
-                <van-button size="mini">010-1234567</van-button>
-            </template>
-        </van-card>
-    </div>
+  <div id="service-container">
+    <bo-navbar text="客服" @left-fun="goback"/>
+    <van-card
+        v-for="item in List"
+        :key="item.id"
+        :desc="item.desc"
+        :title="item.title"
+        :thumb="item.img"
+    >
+      <template #tags>
+        <van-tag plain type="success">{{item.tag}}</van-tag>
+      </template>
+      <template #footer>
+        <van-button size="mini" @click="call(item.phone)">item.phone</van-button>
+      </template>
+    </van-card>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "service",
-        data(){
-            return{
-                from:this.$store.state.currentPosition
-            }
+export default {
+  name: "service",
+  data() {
+    return {
+      from: this.$store.state.currentPosition,
+      List:[
+        {
+          id: 1,
+          title: "客服中心",
+          desc: "负责线上问题投诉建议等处理",
+          img: "https://img.yzcdn.cn/vant/ipad.jpeg",
+          tag: "线上问题",
+          phone: "400-820-8820"
         },
-        methods:{
-            goback(){
-                this.$router.push("/" + this.from + "/my");
-            }
+        {
+          id: 2,
+          title: "防诈中心",
+          desc: "诈骗等违法线下问题处理",
+          img: "https://img.yzcdn.cn/vant/ipad.jpeg",
+          tag: "欺诈问题",
+          phone: "010-1234567"
         }
+
+      ]
     }
+  },
+  methods: {
+    goback() {
+      this.$router.push("/" + this.from + "/my");
+    },
+    call(phone) {
+      window.location.href = `tel:${phone}`;
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-#service-container{
-    h1{
-        font-weight:100;
-    }
+#service-container {
+  h1 {
+    font-weight: 100;
+  }
 }
 </style>
